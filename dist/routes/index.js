f2f255feda0a42241c5c6323c87e8f0b745f8296
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-10-05 22:04:09 +0200

    Serving HTMLS for index and notfound

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,17 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+const path_1 = __importDefault(require("path"));
 const auth_1 = require("./auth");
 const sudoku_1 = require("./sudoku");
 const router = (0, express_1.Router)();
 exports.router = router;
 const API_VERSION = "/api/v1/";
 router.get("/", (req, res) => {
-    res.send("Bienvenido a la API de SUDOKU. Mensaje por escribir.");
+    res.sendFile(path_1.default.join(__dirname, "..", "views", "index.html"));
 });
 // Rutas para la autenticación
 router.use(`${API_VERSION}auth`, auth_1.router);
 // Rutas para sudoku
 router.use(`${API_VERSION}sudoku`, sudoku_1.router);
+router.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "views", "notfound.html"));
+});
 //# sourceMappingURL=index.js.map
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 
 import { Router } from "express";
+import path from "path";
 import { router as authRouter } from "./auth";
 import { router as sudokuRouter } from "./sudoku";
 
@@ -7,7 +8,7 @@ const router = Router();
 const API_VERSION:string = "/api/v1/";
 
 router.get("/", (req, res) => {
-    res.send("Bienvenido a la API de SUDOKU. Mensaje por escribir.");
+    res.sendFile(path.join(__dirname, "..", "views", "index.html"));
   });
   
 // Rutas para la autenticación
@@ -15,5 +16,9 @@ router.use(`${API_VERSION}auth`, authRouter);
 
 // Rutas para sudoku
 router.use(`${API_VERSION}sudoku`, sudokuRouter);
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "views", "notfound.html"));
+  });
 
 export { router };
