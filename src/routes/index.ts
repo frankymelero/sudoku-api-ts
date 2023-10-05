1ed43c2d8775003edb258297df2d7eb46e40921d
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-10-05 18:36:33 +0200

    changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,16 @@ import { router as authRouter } from "./auth";
 import { router as sudokuRouter } from "./sudoku";
 
 const router = Router();
+const API_VERSION:string = "/api/v1/";
 
+router.get("/", (req, res) => {
+    res.send("Bienvenido a la API de SUDOKU. Mensaje por escribir.");
+  });
+  
 // Rutas para la autenticación
-router.use("/api/v1/auth", authRouter);
+router.use(`${API_VERSION}auth`, authRouter);
 
 // Rutas para sudoku
-router.use("/api/v1/sudoku", sudokuRouter);
+router.use(`${API_VERSION}sudoku`, sudokuRouter);
 
 export { router };
